34acf7ffeec9ae84403b4a6a285b282dd0237225
quanghoatrinh <quanghoa.trinh@gmail.com>
2024-05-16 21:07:00 +0700

    categories

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -10,7 +10,7 @@ class HomeController extends Controller
     public function homepage(){
         // select * from products
         // $products = Product::all()->toArray();
-        
+
         // select * from products limit 4
         $products = Product::limit(4)->get()->toArray();
         $categories = [
@@ -23,7 +23,7 @@ class HomeController extends Controller
                 "name"=> "Vegestable"
             ]
         ];
-        return view('welcome',compact("products"));
+        return view('welcome',compact("products","categories"));
     }
 
     public function about_us(){
